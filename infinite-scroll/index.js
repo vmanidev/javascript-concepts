1591419140d2dbe4984e7417c013ed374d27b074
vmanidev <vmanidev@outlook.com>
2025-09-23 21:28:46 +0530

    feat: implement dynamic card generation

---
+++ infinite-scroll/index.js
@@ -0,0 +1,23 @@
+function createCards() {
+
+    const cards = document.getElementById("cards");
+
+    const placeholderObj = {
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In fringilla mattis metus, eu porttitor eros accumsan molestie. Nulla facilisi. Nulla varius urna non turpis dictum, vel hendrerit quam viverra."
+    };
+
+    for (let i = 0; i <= 5; i++) {
+        const div = document.createElement("div");
+        const h3 = document.createElement("h3");
+        const p = document.createElement("p");
+
+        div.append(h3, placeholderObj.title);
+        div.append(p, placeholderObj.description);
+
+        div.setAttribute("class", "card");
+        console.log(cards)
+        cards.appendChild(div);
+
+    }
+};
